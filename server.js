f62e4a100cd73cc9d9954243f131f6e7f68a5984
ycleptkellan <yclepkellan@gmail.com>
2017-06-04 23:03:45 -0700

    express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const helmet = require('helmet')
+const compression = require('compression')
+const port = 3001
+
+app.use(helmet())
+app.use(compression())
+
+app.use('/static', express.static('./dist'))
+
+app.get("/", (req, res) => {
+  res.sendFile( __dirname + '/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`>>> listening on port ${port} `)
+})
